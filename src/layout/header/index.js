166dace7791808ddc0570c2a8ef8cb0f9466f585
lucassousa25 <lucassousao2510@gmail.com>
2022-03-08 20:19:04 -0300

    Add Header Component Layout

---
+++ src/layout/header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import WelcomeCarousel from '../../components/welcome-carousel';
+
+import './styles.css';
+
+const Header = () => {
+
+  return ( 
+    <header id="home">
+      <WelcomeCarousel />
+    </header>
+  )
+}
+
+export default Header;
